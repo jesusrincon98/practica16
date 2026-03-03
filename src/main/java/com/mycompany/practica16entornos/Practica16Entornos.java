d3bd2db346b608e398591e8d279c59b161fa7d82
Jesus Rincon <jesusangelrincon98@gmail.com>
2026-03-03 18:26:40 +0100

    Añadido el hp01

---
+++ src/main/java/com/mycompany/practica16entornos/Practica16Entornos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica16entornos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Practica16Entornos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
